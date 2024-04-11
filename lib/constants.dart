88d5de54a7209dc6f6c8225d1204630ad349c6bb
Adity_Khisa <113276761+adity1234567@users.noreply.github.com>
2024-04-11 12:18:20 +0600

    Delete lib directory

--- lib/constants.dart
+++
@@ -1,4 +0,0 @@
-const String homeRoute = '/';
-const String profileRoute = '/profile';
-const String detailsRoute = '/details';
-const String settingsRoute = '/settings';
--- lib/routes.dart
+++
@@ -1,6 +0,0 @@
-class MyRoutes {
-  static String profileRoute = '/profile';
-   static String homeRoute = '/home';
-    static String widgetRoute = '/widget';
-     static String cameraRoute = '/camera';
-}
